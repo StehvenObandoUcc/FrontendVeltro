78bf3c58fa0c93d1b679b661849184fab6f9d1ca
SoFzzz <paula.gonzalezz@campusucc.edu.co>
2026-03-27 10:00:22 -0500

    feat(11/32): update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import { GlobalErrorBoundary } from './GlobalErrorBoundary';
 import './index.css';
 
 // Create a QueryClient instance with default options
@@ -18,10 +19,12 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <GlobalErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </GlobalErrorBoundary>
   </StrictMode>
 );
